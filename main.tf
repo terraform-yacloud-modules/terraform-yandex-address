81f3b0b5e99ae2bc0eb51d1253670bef22fcad83
Anton Patsev <patsev.anton@gmail.com>
2024-06-30 08:49:29 +0600

    ddos_protection_provider, outgoing_smtp_capability

--- main.tf
+++ main.tf
@@ -3,5 +3,7 @@ resource "yandex_vpc_address" "addr" {
   deletion_protection  = var.deletion_protection == null ? null : var.deletion_protection
   external_ipv4_address {
     zone_id = var.zone_name
+    ddos_protection_provider  = var.ddos_protection_provider == null ? null : var.ddos_protection_provider
+    outgoing_smtp_capability  = var.outgoing_smtp_capability == null ? null : var.outgoing_smtp_capability
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,11 +2,19 @@ variable "ip_address_name" {
   type = string
 }
 
+variable "zone_name" {
+  type        = string
+  description = "Zone name"
+}
+
 variable "deletion_protection" {
   type = string
 }
 
-variable "zone_name" {
-  type        = string
-  description = "Zone name"
+variable "ddos_protection_provider" {
+  type = string
+}
+
+variable "outgoing_smtp_capability" {
+  type = string
 }
