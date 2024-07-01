cb592370fcba49b17cb4989f131f00970c38eb6f
Anton Patsev <patsev.anton@gmail.com>
2024-07-01 10:12:37 +0600

    add output external_ipv4_address

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "external_ipv4_address" {
   description = "Yandex VPC address"
-  value       = yandex_vpc_address.addr.external_ipv4_address.address
+  value       = yandex_vpc_address.addr.external_ipv4_address.address[0]
 }
