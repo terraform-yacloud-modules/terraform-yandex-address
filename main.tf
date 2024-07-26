c9dc66a7ee539cf691355c894c19606ff500c5ac
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2024-07-26 08:02:56 +0600

    add optional dns_record (#8)

--- main.tf
+++ main.tf
@@ -1,12 +1,27 @@
 data "yandex_client_config" "client" {}
 
-resource "yandex_vpc_address" "addr" {
-  name = var.ip_address_name
-  folder_id = var.folder_id == null ? data.yandex_client_config.client.folder_id : var.folder_id
-  deletion_protection  = var.deletion_protection == null ? null : var.deletion_protection
+resource "yandex_vpc_address" "address" {
+  name        = var.name
+  description = var.description
+  labels      = var.labels
+  folder_id   = var.folder_id == null ? data.yandex_client_config.client.folder_id : var.folder_id
+
+  deletion_protection  = var.deletion_protection
+
+
   external_ipv4_address {
-    zone_id = var.zone
-    ddos_protection_provider  = var.ddos_protection_provider == null ? null : var.ddos_protection_provider
-    outgoing_smtp_capability  = var.outgoing_smtp_capability == null ? null : var.outgoing_smtp_capability
+    zone_id                  = var.zone_id
+    ddos_protection_provider = var.ddos_protection_provider
+    outgoing_smtp_capability = var.outgoing_smtp_capability
+  }
+
+  dynamic "dns_record" {
+    for_each = var.dns_record != null ? [var.dns_record] : []
+    content {
+      fqdn        = dns_record.value.fqdn
+      dns_zone_id = dns_record.value.dns_zone_id
+      ttl         = dns_record.value.ttl
+      ptr         = dns_record.value.ptr
+    }
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "external_ipv4_address" {
   description = "Yandex VPC address"
-  value       = yandex_vpc_address.addr.external_ipv4_address[0].address
+  value       = yandex_vpc_address.address.external_ipv4_address[0].address
 }
