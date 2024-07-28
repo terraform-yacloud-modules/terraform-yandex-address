c7ef5ee69f6315b264a73faf5a0438fba909db80
Anton Patsev <patsev.anton@gmail.com>
2024-07-28 19:18:06 +0600

    Исправил некоторые ошибки tflint

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,6 @@
-module "dns-zone" {
+module "dns_zone" {
 
-  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-dns.git//modules/zone"
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-dns.git//modules/zone?ref=v1.0.0"
 
   name        = "my-private-zone"
   description = "desc"
@@ -19,13 +19,13 @@ module "address" {
 
   name        = "test-pip"
   description = "Example address description"
-  labels      = {
+  labels = {
     environment = "production"
   }
   zone_id = "ru-central1-a"
   dns_record = {
     fqdn        = "test"
-    dns_zone_id = module.dns-zone.id
+    dns_zone_id = module.dns_zone.id
     ttl         = 300
     ptr         = true
   }
