4283be4db17a43e826bd5d5ed009fdf3107d3b3d
Anton Patsev <patsev.anton@gmail.com>
2024-07-31 16:46:09 +0600

    Добавил output, README.md

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -1,4 +1,59 @@
+output "id" {
+  description = "ID of the allocated address"
+  value       = module.address.id
+}
+
+output "name" {
+  description = "Name of the allocated address"
+  value       = module.address.name
+}
+
+output "description" {
+  description = "Description of the allocated address"
+  value       = module.address.description
+}
+
+output "folder_id" {
+  description = "Folder ID of the allocated address"
+  value       = module.address.folder_id
+}
+
+output "labels" {
+  description = "Labels of the allocated address"
+  value       = module.address.labels
+}
+
+output "deletion_protection" {
+  description = "Deletion protection flag of the allocated address"
+  value       = module.address.deletion_protection
+}
+
 output "external_ipv4_address" {
   description = "Yandex VPC address"
   value       = module.address.external_ipv4_address
+}
+
+output "external_ipv4" {
+  description = "External IPv4 address details"
+  value       = module.address.external_ipv4_address
+}
+
+output "dns_record" {
+  description = "DNS record details of the allocated address"
+  value       = module.address.dns_record
+}
+
+output "created_at" {
+  description = "Creation timestamp of the allocated address"
+  value       = module.address.created_at
+}
+
+output "reserved" {
+  description = "Indicates if the address is reserved"
+  value       = module.address.reserved
+}
+
+output "used" {
+  description = "Indicates if the address is used"
+  value       = module.address.used
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,59 @@
+output "id" {
+  description = "ID of the allocated address"
+  value       = yandex_vpc_address.address.id
+}
+
+output "name" {
+  description = "Name of the allocated address"
+  value       = yandex_vpc_address.address.name
+}
+
+output "description" {
+  description = "Description of the allocated address"
+  value       = yandex_vpc_address.address.description
+}
+
+output "folder_id" {
+  description = "Folder ID of the allocated address"
+  value       = yandex_vpc_address.address.folder_id
+}
+
+output "labels" {
+  description = "Labels of the allocated address"
+  value       = yandex_vpc_address.address.labels
+}
+
+output "deletion_protection" {
+  description = "Deletion protection flag of the allocated address"
+  value       = yandex_vpc_address.address.deletion_protection
+}
+
 output "external_ipv4_address" {
   description = "Yandex VPC address"
   value       = yandex_vpc_address.address.external_ipv4_address[0].address
+}
+
+output "external_ipv4" {
+  description = "External IPv4 address details"
+  value       = yandex_vpc_address.address.external_ipv4_address
+}
+
+output "dns_record" {
+  description = "DNS record details of the allocated address"
+  value       = yandex_vpc_address.address.dns_record
+}
+
+output "created_at" {
+  description = "Creation timestamp of the allocated address"
+  value       = yandex_vpc_address.address.created_at
+}
+
+output "reserved" {
+  description = "Indicates if the address is reserved"
+  value       = yandex_vpc_address.address.reserved
+}
+
+output "used" {
+  description = "Indicates if the address is used"
+  value       = yandex_vpc_address.address.used
 }
